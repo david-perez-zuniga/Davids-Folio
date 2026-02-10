c1012fcfb6c48561fce1143a64c2eef8e0b03789
David Pérez Zuniga <davidhumbertop503@gmail.com>
2026-02-10 14:22:09 -0600

    Refactor: Modificación de la api para poder mandar el mensaje al correo personal

--- Backend/router/rt_contact.py
+++ Backend/router/rt_contact.py
@@ -1,14 +1,30 @@
 from models.md_contact import ContactForm
 from fastapi import APIRouter
+from fastapi_mail import FastMail, MessageSchema, MessageType
+from pydantic import EmailStr
+from config import conf
 
 router = APIRouter()
 
 @router.post("/contact", response_model= ContactForm)
-def send_data(cont: ContactForm):
+async def send_data(cont: ContactForm):
     new_data = ContactForm(
         name= cont.name,
         email = cont.email,
         user_message= cont.user_message
     )
+
+    html_message = f"""<p>Nuevo mensaje de: {cont.name} {cont.email} </p>
+    <p>El mensaje es: {cont.user_message}</p>"""
+
+    message = MessageSchema(
+        subject="Nuevo mensaje del portafolio",
+        recipients=["davidhumbertop503@gmail.com"],
+        body=html_message,
+        subtype=MessageType.html
+    )
+
+    fm = FastMail(conf)
+    await fm.send_message(message)
 
     return new_data
