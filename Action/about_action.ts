67817fe1da9fa947b2c89ab5d0f02d8f3c93d42a
destripador2000 <davidhumbertop503@gmail.com>
2026-04-23 21:59:51 -0600

    Refactor: Borrando acciones del antiguo código

--- Action/about_action.ts
+++
@@ -1,8 +0,0 @@
-const progress_bar = document.querySelectorAll(".progress-fill");
-
-progress_bar.forEach(bar =>{ 
-    setTimeout(() => {
-        (bar as HTMLElement).style.width = `${bar.getAttribute("data-target")}`
-    }, 100);
-    
-});
